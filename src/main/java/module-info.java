758bed55a2203fadb5899d94fcf4bf9067c932d0
Francesco Mauro <mauro.francesco@educ.di.unito.it>
2022-12-02 18:19:18 +0100

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.progettomailclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.progettomailclient to javafx.fxml;
+    exports com.example.progettomailclient;
+}
