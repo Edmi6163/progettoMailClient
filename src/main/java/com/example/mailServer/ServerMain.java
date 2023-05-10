dbd22e8628417433a64f5a6a69e2f9452c4dde87
Francesco Mauro <mauro.francesco@educ.di.unito.it>
2023-05-10 13:01:26 +0200

    trying to solve the error

--- src/main/java/com/example/mailServer/ServerMain.java
+++ src/main/java/com/example/mailServer/ServerMain.java
@@ -5,91 +5,84 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 import com.example.mailServer.Controller.MailHandler;
 import com.example.mailServer.Controller.ServerHandler;
 import com.example.mailServer.Model.UserList;
-import com.example.mailServer.Controller.ServerLayoutController;
+import javafx.stage.WindowEvent;
 
-import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class ServerMain extends Application {
   @FXML
-  private Stage topStage;
+  private Stage stage;
   @FXML
   private SplitPane rootLayout;
 
   private UserList userList;
   private ObservableList<String> logList = FXCollections.observableArrayList();
 
-  public ObservableList<String> getLogList(){
+  public ObservableList<String> getLogList() {
     return logList;
   }
-  public void addLog(String log){
+
+  public void addLog(String log) {
     logList.add(log);
   }
-  public UserList getUserList(){
+
+  public UserList getUserList() {
     return userList;
   }
 
-  public ServerMain(){
-   userList = new UserList();
-   userList.addUser("francesco@javamail.it");
-   userList.addUser("mauro@javamail.it");
-   userList.addUser("something@javamail.it");
+  public ServerMain() {
+    userList = new UserList();
+    userList.addUser("francesco@javamail.it");
+    userList.addUser("mauro@javamail.it");
+    userList.addUser("something@javamail.it");
   }
 
-  // FIXME: loader path is null dunno why
-  public void initRootLayout(Stage topStage) {
-    System.out.println("initRootLayout function called");
-    try {
-     // FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/ServerLayout.fxml"));
-      FXMLLoader loader = new FXMLLoader(new File("/View/ServerLayout.fxml").toURI().toURL());
-      System.out.println("loader path is "+loader.getClass().getResource("ServerLayout.fxml"));
-      rootLayout = loader.load();
-      ServerLayoutController controller = loader.getController();
-      controller.setServerMain();
 
-      Scene scene = new Scene(rootLayout);
-      topStage.setScene(scene);
-      topStage.show();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
 
 
   private void setUpServer() {
     System.out.println("setUpServer function called"); //TODO debug
     try {
+      int thread_counter = 0;
       ServerSocket s = new ServerSocket(8189);
-      System.out.println("connected to server socket,the ip is "+ Inet4Address.getLocalHost().getHostAddress()+" and the port is 8189");
-      while (true){
+      System.out.println("connected to server socket,the ip is " + Inet4Address.getLocalHost().getHostAddress() + " and the port is 8189");
+      while (true) {
+
         Socket incoming = s.accept();
-        System.out.println("incoming ip is: "+ Inet4Address.getLocalHost().getHostAddress());
-        Runnable r = new ServerHandler(this,incoming,new MailHandler());
-        Thread t = new Thread(r);
-        t.start();
+        System.out.println("incoming ip is: " + Inet4Address.getLocalHost().getHostAddress());
+        Runnable r = new ServerHandler(this, incoming, new MailHandler());
+        new Thread(r).start();
+        thread_counter++;
       }
-    } catch (IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
-  public void start(Stage topStage){
-    this.topStage = topStage;
-    this.topStage.setTitle("Server @javamail");
-    initRootLayout(topStage);
-    Thread t = new Thread(this::setUpServer);
-    t.start();
+  public void start(Stage topStage) throws IOException {
+    try {
+      System.out.println("method: start"); //TODO debug
+      FXMLLoader loader = new FXMLLoader(ServerMain.class.getResource("ServerLayout.fxml"));
+      Scene scene = new Scene(loader.load(), 300, 500);
+      topStage.setTitle("Server log @javamail");
+      topStage.setScene(scene);
+      topStage.show();
+      scene.getWindow().addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, (WindowEvent event) -> System.exit(1));
+      Thread t = new Thread(this::setUpServer);
+      t.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,8 @@ module com.example.progettomailclient {
   requires javafx.fxml;
 
 
-  opens com.example.progettomailclient to javafx.fxml;
   exports com.example.progettomailclient;
   exports com.example.mailServer;
+
+  opens com.example.mailServer to javafx.fxml;
 }
