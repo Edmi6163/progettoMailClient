cb51e90181bd803aca5460f68f66dd161a6ede00
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-05-17 04:01:00 +0200

    view is loaded :D

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,8 @@ module com.example.progettomailclient {
   requires javafx.controls;
   requires javafx.fxml;
 
-
-  exports com.example.progettomailclient;
-  exports com.example.mailServer;
-
   opens com.example.mailServer to javafx.fxml;
-}
+  opens com.example.mailServer.Controller to javafx.fxml;
+
+  exports com.example.mailServer;
+  }
