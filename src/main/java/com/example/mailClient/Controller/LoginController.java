6196090c196bca33745adcefc7a91c70fecac502
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2023-05-21 18:35:08 +0200

    login works now

--- src/main/java/com/example/mailClient/Controller/LoginController.java
+++ src/main/java/com/example/mailClient/Controller/LoginController.java
@@ -14,6 +14,8 @@ import com.example.mailClient.ClientMain;
 import com.example.mailServer.*;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LoginController {
   @FXML
@@ -33,37 +35,52 @@ public class LoginController {
 
 
 
+
+  /*
+  * @brief: using Set datastructure for uniqness of elements, avoiding the need to check each files name
+  * @return: true if the username is in the directory, false otherwise
+  * @note: the username is the name of the file in the directory
+  * */
   @FXML
-  private boolean handleLogin(){
-    System.out.println("username is: " + username.getText());// TODO debug
-    File directory = new File("/home/francesco/Documents/Universita/3 Anno/programmazione3/progettoMailClient/src/main/java/com/example/mailServer/files");
+  private boolean handleLogin() {
+    System.out.println("username is: " + username.getText()); // TODO debug
     String usernameToCheck = this.username.getText() + "@javamail.it";
-    File[] files = directory.listFiles();
-    boolean loginSuccess = false;
-    assert files != null;
-    for(File file : files){
-      System.out.println("file name is " + file.getName());
-      System.out.println("login success status: " + loginSuccess);
-      if(file.getName().equals(usernameToCheck)){
-        loginSuccess = true;
-        logger.setLog(username.getText() + " logged in");
-        System.out.println(username.getText() + " logged in"); //TODO debug, remove
-        System.out.println("login success status: " + loginSuccess); //TODO debug, remove
-        stage.close();
-        break;
-     } else {
-//        System.out.println(username.getText() + " tried to login, but wasn't registered");
-        logger.setLog(username.getText() + " tried to login, but wasn't registered");
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.initOwner(stage);
-        alert.setTitle("wrong username");
-        alert.setHeaderText("An error occurred");
-        alert.setContentText("Can't login");
-        alert.showAndWait();
-        Platform.exit();
-      }
+
+    Set<String> usernames = getUsernamesFromDirectory();
+
+    boolean loginSuccess = usernames.contains(usernameToCheck);
+
+    if (loginSuccess) {
+      logger.setLog(username.getText() + " logged in ");
+      System.out.println(username.getText() + " logged in ");
+      stage.close();
+    } else {
+      logger.setLog(username.getText() + " tried to login, but wasn't registered");
+      Alert alert = new Alert(Alert.AlertType.ERROR);
+      alert.initOwner(stage);
+      alert.setTitle("wrong username");
+      alert.setHeaderText("An error occurred");
+      alert.setContentText("Can't login");
+      alert.showAndWait();
+      Platform.exit();
     }
+
     return loginSuccess;
   }
+
+  private Set<String> getUsernamesFromDirectory() {
+    File directory = new File("/home/francesco/Documents/Università/3_Anno/Prog3/progettoMailClient/src/main/java/com/example/mailServer/file");
+    File[] files = directory.listFiles();
+    Set<String> usernames = new HashSet<>();
+
+    assert files != null;
+    for (File file : files) {
+      usernames.add(file.getName());
+    }
+
+    return usernames;
+  }
+
+
 
 }
